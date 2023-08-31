b7fafa71a025171172d20d2fa5b57b4d3855427c
Slitherings <coop.poop@yahoo.com>
2023-08-31 17:44:54 -0500

    Implementing Email for Contact

--- backend/server.js
+++ backend/server.js
@@ -1,10 +1,13 @@
 const express = require('express');
 const mysql = require('mysql');
 const bodyParser = require('body-parser');
+const nodemailer = require('nodemailer');
 const cors = require('cors');
 
 // Create the Express app
 const app = express();
+
+app.use(bodyParser.urlencoded({ extended: false }));
 
 // Middleware to parse request bodies as JSON
 app.use(bodyParser.json());
@@ -19,6 +22,12 @@ const dbConfig = {
   password: process.env.DB_PASSWORD,
   database: process.env.DB_NAME,
 };
+
+// Email Configuration
+const emailConfig = {
+  email : process.env.EMAIL,
+  password : process.env.EMAILPASSWORD
+}
 
 // Create a MySQL connection pool
 const pool = mysql.createPool(dbConfig);
@@ -47,6 +56,44 @@ app.post('/api/data', (req, res) => {
       res.status(500).json({ error: 'Error executing query' });
     } else {
       res.json({ message: 'Data inserted successfully' });
+    }
+  });
+});
+
+app.post('/send-email', (req, res) => {
+  const { email, phoneNumber, message } = req.body;
+
+  // Create a nodemailer transporter using your email provider's settings
+  const transporter = nodemailer.createTransport({
+    service: 'smtp.gmail.com',
+    port: 587,
+    secure: false,
+    auth: {
+      user: 'xqxqqxqq2@gmail.com',
+      pass: 'wwurbzcnlcledcth'
+    },
+    pool: true, // Add this line
+    direct: true, // Add this line
+  });
+
+  const mailOptions = {
+    from: 'xqxqqxqq2@gmail.com',
+    to: 'JaylenCooper123@yahoo.com',
+    subject: 'New Contact Form Submission',
+    html: `
+      <p>Email: ${email}</p>
+      <p>Phone Number: ${phoneNumber}</p>
+      <p>Message: ${message}</p>
+    `
+  };
+
+  transporter.sendMail(mailOptions, (error, info) => {
+    if (error) {
+      console.log('Error sending email:', error);
+      res.status(500).send('Error sending email');
+    } else {
+      console.log('Email sent:', info.response);
+      res.status(200).send('Email sent successfully');
     }
   });
 });
